d12716ef20e28f125d754c9ba4bbfdddfee518ce
rutul <prajapatirutul1223@gmail.com>
2025-02-14 13:22:48 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const cors = require("cors");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 9000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
