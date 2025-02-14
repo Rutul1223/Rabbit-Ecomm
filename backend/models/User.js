155dae58cc362ae5c8063a4cf26fd691fa15e289
rutul <prajapatirutul1223@gmail.com>
2025-02-14 15:04:22 +0530

    user model created

---
+++ backend/models/User.js
@@ -0,0 +1,52 @@
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
+
+// Password Hash Middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// match user entered password to hashed password in database
+userSchema.methods.isPasswordMatch = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
