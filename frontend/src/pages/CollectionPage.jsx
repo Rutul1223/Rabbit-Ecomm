67f836f6c805da9fb8042f01f7e45998306d635b
rutul <prajapatirutul1223@gmail.com>
2025-02-21 14:49:48 +0530

    dynamic data fetching added

--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -3,11 +3,22 @@ import { FaFilter } from "react-icons/fa6";
 import FilterSidebar from "../components/Products/FilterSidebar";
 import SortOptions from "../components/Products/SortOptions";
 import ProductGrid from "../components/Products/ProductGrid";
-
+import { useParams, useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProductsByFilters } from "../redux/slices/productsSlice";
 const CollectionPage = () => {
-  const [products, setProducts] = useState([]);
+  const { collection } = useParams();
+  const [searchParams] = useSearchParams();
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector((state) => state.products);
+  const queryParams = Object.fromEntries([...searchParams]);
+
   const sidebarRef = useRef(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    dispatch(fetchProductsByFilters({ collection, ...queryParams }));
+  }, [dispatch, collection, searchParams]);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -25,94 +36,6 @@ const CollectionPage = () => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
-
-  useEffect(() => {
-    setTimeout(() => {
-      const fetchedProducts = [
-        {
-          _id: 1,
-          name: "Product 1",
-          price: 120,
-          images: [
-            {
-              url: "https://muselot.in/cdn/shop/products/Plaintshirtsforwomenonline-Greyplaintshirtforwomen-Muselot_1.jpg?v=1657571397",
-            },
-          ],
-        },
-        {
-          _id: 2,
-          name: "Product 2",
-          price: 120,
-          images: [
-            {
-              url: "https://images.meesho.com/images/products/348858433/r5alq_512.webp",
-            },
-          ],
-        },
-        {
-          _id: 3,
-          name: "Product 3",
-          price: 120,
-          images: [
-            {
-              url: "https://static.cilory.com/406638-thickbox_default/estonished-olive-shirt.jpg",
-            },
-          ],
-        },
-        {
-          _id: 4,
-          name: "Product 4",
-          price: 120,
-          images: [
-            {
-              url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOpH_zNx4WDZes9v4qlNKkIUYSJEepV7OWGg&s",
-            },
-          ],
-        },
-        {
-          _id: 5,
-          name: "Product 5",
-          price: 120,
-          images: [
-            {
-              url: "https://muselot.in/cdn/shop/products/Plaintshirtsforwomenonline-Greyplaintshirtforwomen-Muselot_1.jpg?v=1657571397",
-            },
-          ],
-        },
-        {
-          _id: 6,
-          name: "Product 6",
-          price: 120,
-          images: [
-            {
-              url: "https://images.meesho.com/images/products/348858433/r5alq_512.webp",
-            },
-          ],
-        },
-        {
-          _id: 7,
-          name: "Product 7",
-          price: 120,
-          images: [
-            {
-              url: "https://static.cilory.com/406638-thickbox_default/estonished-olive-shirt.jpg",
-            },
-          ],
-        },
-        {
-          _id: 8,
-          name: "Product 8",
-          price: 120,
-          images: [
-            {
-              url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOpH_zNx4WDZes9v4qlNKkIUYSJEepV7OWGg&s",
-            },
-          ],
-        },
-      ];
-      setProducts(fetchedProducts);
-    }, 1000);
   }, []);
 
   return (
@@ -141,7 +64,7 @@ const CollectionPage = () => {
         <SortOptions />
 
         {/* Product Grid Options */}
-        <ProductGrid products={products} />
+        <ProductGrid products={products} loading={loading} error={error} />
       </div>
     </div>
   );
