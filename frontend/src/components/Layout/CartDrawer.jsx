94586c7608a5501d35cce39075a9b6b01f77302d
rutul <prajapatirutul1223@gmail.com>
2025-02-21 17:16:58 +0530

    updated

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -2,14 +2,21 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoMdClose } from "react-icons/io";
 import CartContents from "../Cart/CartContents";
+import { useSelector } from "react-redux";
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
   const navigate = useNavigate();
-
+  const { user, guestId } = useSelector((state) => state.auth);
+  const { cart } = useSelector((state) => state.cart);
+  const userId = user ? user._id : null;
   const handleCheckout = () => {
     toggleCartDrawer();
-    navigate("/checkout");
-  }
+    if (!user) {
+      navigate("/login?redirect=/checkout");
+    } else {
+      navigate("/checkout");
+    }
+  };
   return (
     <div
       className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
@@ -25,22 +32,28 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
 
       {/* Cart contents with scrollable area */}
       <div className="flex-grow p-4 overflow-y-auto">
-        <h1 className="text-xl font-semibold mb-4">Your Cart</h1>
-
-        {/* Component for cart contents */}
-        <CartContents />
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        {cart && cart?.products?.length > 0 ? (
+          <CartContents cart={cart} userId={userId} guestId={guestId} />
+        ) : (
+          <p className="text-gray-600">Your cart is empty.</p>
+        )}
       </div>
       {/* Checkout button fixed at the bottom*/}
       <div className="p-4 bg-white sticky bottom-0">
-        <button
-          onClick={handleCheckout}
-          className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"
-        >
-          Checkout
-        </button>
-        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
-          Shipping, taxes, and discount codes calculated at checkout.
-        </p>
+        {cart && cart?.products?.length > 0 && (
+          <>
+            <button
+              onClick={handleCheckout}
+              className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"
+            >
+              Checkout
+            </button>
+            <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
+              Shipping, taxes, and discount codes calculated at checkout.
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
