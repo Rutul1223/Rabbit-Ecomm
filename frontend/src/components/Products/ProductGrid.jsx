6af876e91ae3bf6208910959972ef48f60f0a0af
rutul <prajapatirutul1223@gmail.com>
2025-02-21 13:00:11 +0530

    loading and error mehtod added

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProductGrid = ({ products }) => {
+const ProductGrid = ({ products, loading, error }) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-      {products.map((products, index) => (
+      {products?.map((products, index) => (
         <Link key={index} to={`/products/${products._id}`} className="block">
           <div className="bg-white p-4 rounded-lg">
             <div className="w-full h-96 mb-4">
