ace90f514230b8c9b06be1a1b217005bbde10737
rutul <prajapatirutul1223@gmail.com>
2025-02-21 16:45:34 +0530

    price added

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -9,6 +9,7 @@ const cartItemSchema = new mongoose.Schema(
         },
         name: String,
         image: String,
+        price: Number,
         color: String,
         size: String,
         quantity: {
