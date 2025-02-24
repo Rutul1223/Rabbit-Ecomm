48451025bef56d6c4c89da869d813ed4c357510f
rutul <prajapatirutul1223@gmail.com>
2025-02-24 15:48:59 +0530

    admin pages updated

--- frontend/src/components/Admin/EditProductPage.jsx
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  fetchProductDetails,
+  updateProduct,
+} from "../../redux/slices/productsSlice";
 
 const EditProductPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { selectedProduct, loading, error } = useSelector(
+    (state) => state.products
+  );
   const [productData, setProductData] = useState({
     name: "",
     description: "",
@@ -14,15 +26,22 @@ const EditProductPage = () => {
     collections: "",
     material: "",
     gender: "",
-    image: [
-      {
-        url: "https://static.cilory.com/406638-thickbox_default/estonished-olive-shirt.jpg",
-      },
-      {
-        url: "https://static.cilory.com/406638-thickbox_default/estonished-olive-shirt.jpg",
-      },
-    ],
+    image: [],
   });
+
+  const [uploading, setUploading] = useState(false); //Image uploading state
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchProductDetails(id));
+    }
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (selectedProduct) {
+      setProductData(selectedProduct);
+    }
+  }, [selectedProduct]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -34,13 +53,36 @@ const EditProductPage = () => {
 
   const handleImageUpload = async (e) => {
     const file = e.target.files[0];
-    console.log(file);
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      setUploading(true);
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/upload`,
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      setProductData((prevData) => ({
+        ...prevData,
+        image: [...prevData.image, { url: data.imageUrl, altText: "" }],
+      }));
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(productData);
-  }
+    dispatch(updateProduct({ id, productData }));
+    navigate("/admin/products");
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="max-w-5xl mx-auto p-6 shadow-md rounded-md">
@@ -151,7 +193,7 @@ const EditProductPage = () => {
             className="w-full border border-gray-300 rounded-md p-2"
           />
           <div className="flex gap-4 mt-4">
-            {productData.image.map((image, index) => (
+            {productData.image?.map((image, index) => (
               <div key={index}>
                 <img
                   src={image.url}
