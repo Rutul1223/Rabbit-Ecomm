42556d70661579e4a9e7c7e062b5979bcaaba6f8
rutul <prajapatirutul1223@gmail.com>
2025-02-24 14:02:21 +0530

    dynamic orders fetched

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,48 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { fetchUserOrders } from "../redux/slices/orderSlice";
 
 const MyOrdersPage = () => {
-  const [orders, setOrders] = useState([]);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { orders, loading, error } = useSelector((state) => state.orders);
+
   useEffect(() => {
-    setTimeout(() => {
-      const mockOrders = [
-        {
-          _id: "12345",
-          createdAt: new Date(),
-          shippingAddress: { city: "New York", country: "USA" },
-          orederItems: [
-            {
-              name: "Product 1",
-              image:
-                "https://muselot.in/cdn/shop/products/Plaintshirtsforwomenonline-Greyplaintshirtforwomen-Muselot_1.jpg?v=1657571397",
-            },
-          ],
-          totalPrice: 100,
-          isPaid: true,
-        },
-        {
-          _id: "5678",
-          createdAt: new Date(),
-          shippingAddress: { city: "Ahmedabad", country: "India" },
-          orederItems: [
-            {
-              name: "Product 2",
-              image:
-                "https://muselot.in/cdn/shop/products/Plaintshirtsforwomenonline-Greyplaintshirtforwomen-Muselot_1.jpg?v=1657571397",
-            },
-          ],
-          totalPrice: 300,
-          isPaid: true,
-        },
-      ];
-      setOrders(mockOrders);
-    }, 1000);
-  }, []);
+    dispatch(fetchUserOrders());
+  }, [dispatch]);
 
   const handleRowClick = (orderId) => {
     navigate(`/order/${orderId}`);
   };
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <div className="max-w-7xl mx-auto p-4 sm:p-6">
