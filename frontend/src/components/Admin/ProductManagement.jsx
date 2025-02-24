620262aa642ade33a79f911731348c4746582ad3
rutul <prajapatirutul1223@gmail.com>
2025-02-24 15:49:17 +0530

    admin page updated

--- frontend/src/components/Admin/ProductManagement.jsx
+++ frontend/src/components/Admin/ProductManagement.jsx
@@ -1,21 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  deleteProduct,
+  fetchAdminProducts,
+} from "../../redux/slices/adminProductSlice";
 
 const ProductManagement = () => {
-  const products = [
-    {
-      _id: 1,
-      name: "Product 1",
-      price: 101,
-      sku: "123123123",
-    },
-  ];
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector(
+    (state) => state.adminProducts
+  );
 
+  useEffect(() => {
+    dispatch(fetchAdminProducts());
+  }, [dispatch]);
   const handleDelete = (id) => {
     if (window.confirm("Are you sure you want to delete the product?")) {
-      console.log("Delete product with ID:", id);
+      dispatch(deleteProduct(id));
     }
   };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="max-w-7xl mx-auto p-6">
@@ -34,7 +41,7 @@ const ProductManagement = () => {
             {products.length > 0 ? (
               products.map((product) => (
                 <tr
-                  key={product.id}
+                  key={product._id}
                   className="border-b hover:bg-gray-50 cursor-pointer"
                 >
                   <td className="p-4 font-medium text-gray-900 whitespace-nowrap">
@@ -50,7 +57,7 @@ const ProductManagement = () => {
                       Edit
                     </Link>
                     <button
-                      onClick={() => handleDelete()}
+                      onClick={() => handleDelete(product._id)}
                       className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
                     >
                       Delete
